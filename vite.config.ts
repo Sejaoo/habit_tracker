9435e1db64f666394fa0814345ce4ad32b79793a
SeannnnRowe <seanrowe0904@gmail.com>
2025-04-07 15:35:37 -0500

    Update avatar paths and other config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import path from 'path'; // Removed unused import
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   css: { postcss: './postcss.config.cjs' },
+  base: '/habit_tracker/'
 });
